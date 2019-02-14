c6d16f48b465ed9ab0f31a26305e477142e1a7db
AJHudson <02andrewjames.hudson@gulllakecs.org>
2019-02-14 09:33:46 -0500

    add functions and variables

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -1,9 +1,27 @@
 # 4.13.3: greetings
 # Aj Hudson
 # 2.6.19
+
+'''
 name = input("What is your name: ")
 def greeting():
     print(" hi there " + name + "!")
     print("nice to meet you")
 
 greeting()
+'''
+
+# 4.13.4: Functions and Variables
+# AJ
+# 2.14.19
+
+x = 500
+
+
+def print_something():
+    x = 15
+    print(x)
+
+print_something()
+
+print(x)
