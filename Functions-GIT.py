9cf7a72d9349fc14a96513bce853bbe30ea97f3e
AJHudson2003 <48022408+AJHudson2003@users.noreply.github.com>
2019-02-28 09:16:34 -0500

    add print multiple times

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -1,8 +1,6 @@
 # 4.13.3: greetings
 # Aj Hudson
 # 2.6.19
-
-
 name = input("What is your name: ")
 def greeting():
     print(" hi there " + name + "!")
@@ -10,11 +8,9 @@ def greeting():
 
 greeting()
 
-
 # 4.13.4: Functions and Variables
 # AJ
 # 2.14.19
-
 x = 676867
 
 def print_something():
@@ -28,7 +24,6 @@ print(x)
 # 4.13.5: functions and variables pt2
 # aj
 # 2.14.19
-
 my_variables = 4.5659
 
 def something():
@@ -39,7 +34,6 @@ something()
 # 4.13.6: functions & Variables pt 3
 # 2.18.19
 # aj hudson
-
 def print_number(x):
     print(str(x))
 print_number(15)
@@ -55,10 +49,16 @@ name_age("james", 33)
 
 # 4.14.5: default parameter values
 # aj 2.19.19
-
 def print_two_numbers(x, y = 20):
     print('first number: ', x)
     print('second number: ', y)
 print_two_numbers(15, 36)
-
 print_two_numbers(56)
+
+# 4.14.7 print multiple times
+# aj 2.19.19
+def print_multiple_times(string, times):
+    for i in range(times):
+        print(string)
+
+print_multiple_times('I like games', 5)
